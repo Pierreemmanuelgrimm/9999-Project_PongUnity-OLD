1c410814c4014fd8fe674fab9c0df89f4a362a7d
Pierre-Emmanuel Grimm <pierreemmanuel.grimm@gmail.com>
2018-02-11 14:06:20 +0000

    Fix Part 2

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerForce = 200f;
+    Vector3 lastPosition;
+	// Update is called once per frame
+	void FixedUpdate () {
+        lastPosition = transform.position;
+        if (Input.GetKey("a"))
+        {
+            //transform.Translate(0, 0, playerForce * Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(0, 0, playerForce*Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            //transform.Translate(0, 0, -playerForce * Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(0, 0, -playerForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+
+}
